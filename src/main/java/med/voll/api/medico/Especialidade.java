ee3cc99a172f2c078efedf488a4a973525bd2898
Gabriel Oliveira <gabriellfernandoo@gmail.com>
2024-01-08 11:05:00 -0300

    initial commit

---
+++ src/main/java/med/voll/api/medico/Especialidade.java
@@ -0,0 +1,17 @@
+package med.voll.api.medico;
+
+public enum Especialidade {
+    CARDIOLOGIA,
+    CLINICA_GERAL,
+    DERMATOLOGIA,
+    ENDOCRINOLOGIA,
+    GASTROENTEROLOGIA,
+    GINECOLOGIA,
+    NEUROLOGIA,
+    OFTALMOLOGIA,
+    ORTOPEDIA,
+    OTORRINOLARINGOLOGIA,
+    PEDIATRIA,
+    PSIQUIATRIA,
+    UROLOGIA
+}
